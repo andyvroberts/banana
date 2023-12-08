a01cbec3f609a9fe0c0f0adb4b932a0d813ae388
Andy Roberts <avroberts@outlook.com>
2023-12-08 20:50:57 +0000

    fact load CTE to inline view

--- src/81-load-prices.sql
+++ src/81-load-prices.sql
@@ -3,30 +3,33 @@
 -- 
 -- 
 -- ----------------------------------------------------------------------------
-WITH 
-    price_date AS
-    (
-        SELECT id, date_yyyymmdd FROM business_date
-    ),
-    property_location AS
-    (
-        SELECT id, paon, saon, street, postcode, town, district, county
-            FROM property_location
-    )
 INSERT INTO property_price
-    (price_date_id, property_location_id, price)
+    (price_date_id, property_location_id, property_address_id, price)
 SELECT
-    pd.id, pl.id, pp.price 
+    ad.id, al.id, aa.id, pp.price 
 FROM 
     prices_csv pp 
-INNER JOIN
-    price_date pd ON pd.date_yyyymmdd = COALESCE(FORMAT(pp.price_date, 'yyyyMMdd'), NULL)
-INNER JOIN 
-    property_location pl ON pl.paon = COALESCE(pp.paon, NULL) AND
-                            pl.saon = COALESCE(pp.saon, NULL) AND
-                            pl.street = COALESCE(pp.street, NULL) AND 
-                            pl.postcode = COALESCE(pp.postcode, NULL) AND 
-                            pl.town = COALESCE(pp.town, NULL) AND 
-                            pl.district = COALESCE(pp.district, NULL) AND
-                            pl.county = COALESCE(pp.county, NULL)
+LEFT OUTER JOIN
+    (
+        SELECT id, date_yyyymmdd FROM business_date
+    ) 
+    AS ad ON ad.date_yyyymmdd = COALESCE(FORMAT(pp.price_date, 'yyyyMMdd'), NULL)
+LEFT OUTER JOIN 
+    (
+        SELECT id, postcode, locality FROM property_location
+    )
+    AS al ON ISNULL(al.postcode, 1) = ISNULL(pp.postcode, 1) AND 
+             ISNULL(al.locality, 1) = ISNULL(pp.locality, 1)
+LEFT OUTER JOIN 
+    (
+        SELECT id, paon, saon, street, postcode, town, district, county
+        FROM property_address
+    )
+    AS aa ON ISNULL(aa.paon, 1) = ISNULL(pp.paon, 1) AND
+             ISNULL(aa.saon, 1) = ISNULL(pp.saon, 1) AND
+             ISNULL(aa.street, 1) = ISNULL(pp.street, 1) AND 
+             ISNULL(aa.postcode, 1) = ISNULL(pp.postcode, 1) AND 
+             ISNULL(aa.town, 1) = ISNULL(pp.town, 1) AND 
+             ISNULL(aa.district, 1) = ISNULL(pp.district, 1) AND 
+             ISNULL(aa.county, 1) = ISNULL(pp.county, 1)
 ;
